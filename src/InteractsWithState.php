575609033656ecb74abbed01b49370752f3073f0
Muhammed Sari <muhammed@dive.be>
2021-05-03 14:56:11 +0200

    Delegate calls

--- src/InteractsWithState.php
+++ src/InteractsWithState.php
@@ -19,4 +19,17 @@ trait InteractsWithState
 
         return $this;
     }
+
+    public function canTransitionTo(string $state): bool
+    {
+        return $this->state->canTransitionTo($state);
+    }
+
+    /**
+     * @throws Exceptions\TransitionFailedException
+     */
+    public function transitionTo(string $to): self
+    {
+        return $this->state->transitionTo($to);
+    }
 }
